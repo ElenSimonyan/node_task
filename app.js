e5865964a45acdc8c2e4644bded2a853afa9c9c7
ElenSimonyan <simonyanelen15@gmail.com>
2017-10-03 18:37:28 +0400

    updated version

--- app.js
+++ app.js
@@ -3,16 +3,25 @@
 const http = require('http');
 
 
+const func = (url,i) => {
+    const result = {};
+    const textSplit = url[1].split('&');
+    const key = textSplit[i].split('=')[0];
+    const value = textSplit[i].split('=')[1];
+    result[key] = value;
+    return (`${result[key]} `);
+}
+
 const server = http.createServer((req, res) => {
     const split = req.url.split('?');
 
     if ('/search' === split[0]) {
-        const textSplit = split[1].split('=')[1];
-        const firstValue = textSplit.split('&')[0];
-        const filter = split[1].split('&')[1];
-        const secondValue = filter.split('=')[1];
+        const query = split[1].split('&');
 
-        res.write(`${firstValue} ${secondValue}`)
+        for (let i = 0; i < query.length; i++ ){
+            res.write(func(split,i))
+        }
+
     }
     res.end();
 });
